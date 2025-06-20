c178ecbe6b7b4df2a94b16de31dadb0c0f6e24d2
Timothee Dietz Romero <tdietz-r@3-D-12.42heilbronn.de>
2025-06-21 00:14:48 +0200

    radix sort for arraysize more then 5

--- algorithm/sort_big.c
+++ algorithm/sort_big.c
@@ -6,17 +6,127 @@
 /*   By: tdietz-r <tdietz-r@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 18:25:24 by tdietz-r          #+#    #+#             */
-/*   Updated: 2025/06/16 15:25:55 by tdietz-r         ###   ########.fr       */
+/*   Updated: 2025/06/21 00:10:28 by tdietz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+static void	copy_stack_to_array(t_stack *stack, int *sorted_copy)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack->len_a)
+	{
+		sorted_copy[i] = stack->stack_a[i];
+		i++;
+	}
+}
+
+static void	bubble_sort_array(int *array, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+static void	replace_values_with_indices(t_stack *stack, int *sorted_copy)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < stack->len_a)
+	{
+		j = 0;
+		while (j < stack->len_a)
+		{
+			if (stack->stack_a[i] == sorted_copy[j])
+			{
+				stack->stack_a[i] = j;
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+static void	normalize_to_index(t_stack *stack)
+{
+	int	*sorted_copy;
+
+	sorted_copy = malloc(stack->len_a * sizeof(int));
+	if (!sorted_copy)
+		return ;
+	copy_stack_to_array(stack, sorted_copy);
+	bubble_sort_array(sorted_copy, stack->len_a);
+	replace_values_with_indices(stack, sorted_copy);
+	free(sorted_copy);
+}
+
+int	max_bits(t_stack *stack)
+{
+	int	max;
+	int	bits;
+	int	i;
+
+	max = stack->stack_a[0];
+	bits = 0;
+	i = 1;
+	while (i < stack->len_a)
+	{
+		if (stack->stack_a[i] > max)
+			max = stack->stack_a[i];
+		i++;
+	}
+	while ((max >> bits) != 0)
+		bits++;
+	return (bits);
+}
+
 void	k_sort(t_stack *stack)
 {
-	int	a;
+	int	bits;
+	int	size;
+	int	i;
+	int	j;
 
-	a = 0;
-	a = 1;
-	return ;
+	normalize_to_index(stack);
+	bits = max_bits(stack);
+	size = stack->len_a;
+	i = 0;
+	while (i < bits)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (((stack->stack_a[0] >> i) & 1) == 0)
+				pb_push(stack);
+			else
+				ra_rotate(stack);
+			j++;
+		}
+		while (stack->len_b > 0)
+			pa_push(stack);
+		i++;
+	}
 }
